91c2b2738f914c13c2fce155752c69390d76a05f
Ruslan Bilotkach <rbilotkach@gmail.com>
2024-01-24 15:50:28 -0700

    small changes
    + add permissions
    + add 48x48 and 96x96 icons
    - remove canius.js contents to prepare for the shift in project direction

--- canius.js
+++ canius.js
@@ -1,10 +1,3 @@
 
-// selects the html section element which contains the new article
-const section = document.querySelector(
-    'section.collapsible-section.ds-layout[data-selection-id="topstories"]'
-);
+console.log("extension running...");
 
-// deletes said section
-if (section) {
-    section.remove();
-} 
